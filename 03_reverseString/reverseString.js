da0baf8b7faee7ba6e0a6fc34c634c9354499f47
hugo salazar <hugog.salazarl@gmail.com>
2024-11-02 10:11:34 -0500

    exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let reversedWord = word.split("").reverse().join("");
+  return reversedWord
 };
 
 // Do not edit below this line
