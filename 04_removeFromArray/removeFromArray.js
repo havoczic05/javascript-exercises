ee94dad892d089acbcda40263b5672e0b70c7524
hugo salazar <hugog.salazarl@gmail.com>
2024-11-02 11:46:31 -0500

    exercise completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  return array.filter(function (val) {
+    return !args.includes(val);
+  })
 };
 
 // Do not edit below this line
