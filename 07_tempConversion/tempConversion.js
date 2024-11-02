6bb920c399b4c07e2e93e1bb7a73d965b600f084
hugo salazar <hugog.salazarl@gmail.com>
2024-11-02 15:43:39 -0500

    exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+  const cTemp = (fTemp - 32) / (9 / 5)
+  if (cTemp % 1 == 0) {
+    return cTemp
+  }
+
+  return parseFloat(cTemp.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  const fTemp = (cTemp * (9 / 5)) + 32
+  return parseFloat(fTemp.toFixed(1))
 };
 
 // Do not edit below this line
